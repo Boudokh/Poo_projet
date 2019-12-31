b60081afa3afb85e08d26b9f3d1f42a7908a2f57
Boudokh <mboudokhane@gmail.com>
2019-12-31 02:25:36 +0100

    file selection working

--- g_player.cpp
+++ g_player.cpp
@@ -7,9 +7,7 @@ char getMove();
 
 int main(int argc, char *argv[])
 {
-    Game *new_game = new Game(15, 30, 5, 6, 3, 2, 2);
-
-    std::regex is_board("([a-zA-Z0-9_])+(.cpp)$");
+    std::regex is_board("([a-zA-Z0-9_])+(.board)$");
 
     std::string tmp_fname;
     struct dirent *dir;
@@ -33,11 +31,43 @@ int main(int argc, char *argv[])
 
     initscr();
     int i = 0;
+    if (file_list.size() == 0)
+    {
+        printw("Aucun fichier .board détécté, veuillez en créer grace au fichier \'gc\'\nAppuyez sur n'importe quelle touche pour sortir");
+        getch();
+        endwin();
+        return 0;
+    }
     for (std::vector<std::string>::iterator it = file_list.begin(); it != file_list.end(); it++, i++)
     {
-        mvprintw(i, 0, "%2d. %s", i + 1, it->c_str());
+        mvprintw(i, 0, " %2d. %s", i + 1, it->c_str());
     }
-    getch();
+    move(0, 0);
+    keypad(stdscr, true);
+
+    int input, x, y;
+
+    do
+    {
+        input = getch();
+        getyx(stdscr, y, x);
+        switch (input)
+        {
+        case KEY_UP:
+            move(std::max(0, y - 1), 0);
+            break;
+        case KEY_DOWN:
+            move(std::min(int(file_list.size()), y + 1), 0);
+            break;
+        default:
+            break;
+        }
+    } while (input != 10);
+
+    getyx(stdscr, y, x);
+
+    Game *new_game = new Game(file_list[y]);
+
     char next_move = '0';
     int game_state = 0;
 
