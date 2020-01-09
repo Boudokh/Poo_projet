5d1716b52abee9ed94a09beb1956f1e5c02cf545
Boudokh <mboudokhane@gmail.com>
2020-01-09 03:21:27 +0100

    .game and .board split (#24)

--- board_gen.cpp
+++ board_gen.cpp
@@ -5,23 +5,26 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
-    int lar = 20;
-    int hau = 12;
+    int lar = 50;
+    int hau = 20;
     int nb_level = 5;
     int teupor = 10;
     int diam = 5;
-    int streum = 6;
+    int streum = 4;
     int geurchar = 5;
 
     Game *a = new Game(hau, lar, nb_level, teupor, diam, streum, geurchar);
 
     //a->playStreumons();
     //a->affiche();
-    a->play();
+    //a->save_boards("aa");
+    //a->play();
+    //a->save_game("aaa");
     //a->save_game();
 
-    std::string filename = "jeu.txt";
-    Game *b = new Game(filename);
+    Game *b = new Game("aa.board");
     //b->save_game();
-    //b->play();
+    b->play();
+    b = new Game("aaa.game");
+    b->play();
 }
