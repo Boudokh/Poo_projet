2afd5e72d721a276a65e8e12a4f8d32050937a35
Boudokh <mboudokhane@gmail.com>
2020-01-09 17:54:33 +0100

    denser walls, correction player

--- board_gen.cpp
+++ board_gen.cpp
@@ -5,26 +5,14 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
-    int lar = 50;
-    int hau = 20;
-    int nb_level = 5;
+    int lar = 30;
+    int hau = 5;
+    int nb_level = 100;
     int teupor = 10;
-    int diam = 5;
-    int streum = 4;
-    int geurchar = 5;
+    int diam = 0;
+    int streum = 0;
+    int geurchar = 0;
 
     Game *a = new Game(hau, lar, nb_level, teupor, diam, streum, geurchar);
-
-    //a->playStreumons();
-    //a->affiche();
-    //a->save_boards("aa");
-    //a->play();
-    //a->save_game("aaa");
-    //a->save_game();
-
-    Game *b = new Game("aa.board");
-    //b->save_game();
-    b->play();
-    b = new Game("aaa.game");
-    b->play();
+    a->affiche();
 }
