90d880a2f4ae6ddf5c7417bd3f9af53da70d86e6
Mohamed-Amine <m.bousahih@gmail.com>
2020-01-12 00:17:40 +0100

    test

--- Oueurj.cpp
+++ Oueurj.cpp
@@ -1,5 +1,15 @@
 #include "Oueurj.hpp"
 
+/**
+ * @brief constructeur d'un oueurj
+ * 
+ * @param _pos position du oueurj
+ * @param _nb_diams nombre de diams
+ * @param _inf_telep option teleportation infinie active 
+ * @param _nb_teleport nombre de teleportation
+ * @param _state etat actuel du oueurj
+ */
+
 Oueurj::Oueurj(std::vector<int> _pos, int _nb_diams, bool _inf_telep, int _nb_teleport, int _state) : pos(_pos), nb_diams(_nb_diams), inf_telep(_inf_telep), nb_teleport(_nb_teleport), state(_state)
 {
     this->symbol = 'J';
@@ -79,6 +89,12 @@ int Oueurj::getNbDiams()
     return this->nb_diams;
 }
 
+/**
+ * @brief affichage sur la console du nombre de téléportation, de diams et du niveau courant à droite du plateau.
+ * 
+ * @return std::stringstream 
+ */
+
 std::stringstream Oueurj::toStream() const
 {
     std::stringstream score_line;
@@ -88,6 +104,11 @@ std::stringstream Oueurj::toStream() const
     return score_line;
 }
 
+/**
+ * @brief activation de l'option teleportation infinie
+ * 
+ */
+
 void Oueurj::switch_teleport()
 {
     this->inf_telep = true;
@@ -95,6 +116,11 @@ void Oueurj::switch_teleport()
 
 int Oueurj::getCurrentlevel() const { return this->pos[0]; }
 
+/**
+ * @brief état courant du oueurj
+ * 
+ * @return int 
+ */
 int Oueurj::getState() const { return this->state; }
 
 Oueurj::~Oueurj() {}
--- Oueurj.hpp
+++ Oueurj.hpp
@@ -8,33 +8,54 @@ class Oueurj : public Object
 {
 
 private:
-    std::vector<int> pos;
+    std::vector<int> pos; // position du oueurj (label plateau, i , j)
     int nb_diams;
-    bool inf_telep;
+    bool inf_telep; // teleportation infinie
     int nb_teleport;
     int state; // 0 en jeu, -1 mort, 1 gagné
 
 public:
+
     Oueurj(std::vector<int> _pos, int _nb_diams = 0, bool _inf_telep = false, int _nb_teleport = 0, int _state = 0);
     Oueurj();
     ~Oueurj();
 
     void levelUp();
 
+    // déplacement du oueurj sur le plateau.
     void setPos(const std::vector<int> new_pos);
+
+    // récupération de la position du oueurj sur le plateau.
     std::vector<int> getPos() const;
+
     int getCurrentlevel() const;
+
+    // utilisation de la téléportation.
+
     bool teleport();
+
+    // activation de la téléportation.
+
     void switch_teleport();
 
     void die();
     void win();
 
+    // affichage du nombre de téléportations.
+
     std::string getTelep() const;
+
+
     int getNbDiams();
 
+    // état courant du oueurj.
+
     int getState() const;
+
+    // affichage du nombre de téléportations, de diams et du niveau actuel.
+
     std::stringstream toStream() const;
+
     void eatDiams();
 };
 
